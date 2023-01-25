70ff4d9bb8cb0a7fbd432e1b5b2499e90efbf7e0
Kovács Anna <anna.kovacs@stud.uni-obuda.hu>
2023-01-25 16:29:25 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>LOL</h1>
+      {/* Header */}
+      <Header />
+      {/* app body */}
+
+      {/* sidebar */}
+
+      {/* feed */}
+
+      {/* widget */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+export default function 
+
+() {
+  return (
+    <div className='h'>
+        <h1>This is the Header</h1>
+
+    </div>
+  )
+}
